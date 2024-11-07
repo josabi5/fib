fd2f4aa10a07c799a413a0615648e0131af8e679
josabi5 <josabiedma@uma.es>
2024-11-07 20:42:26 +0100

    fix: rename fib with memoization

---
+++ fib.py
@@ -0,0 +1,55 @@
+import argparse
+import sys
+
+
+def fibonacci_iterative(n: int) -> int:
+    """
+    Computes the n-th Fibonacci number.
+    :param n: n-th Fibonacci numner,
+    :return: Ther m-th Fibonacci number
+    """
+    if n < 0:
+        raise ValueError("n must be gerater than or equal to 0.")
+    if n < 2:
+        return n
+    n0 = 0
+    result1 = 0
+    n1 = 1
+
+    for i in range(n - 1):
+        result1 = n0 + n1
+        n0 = n1
+        n1 = result1
+    return result1
+
+
+cache = {}  # esto es para guardar los valoers que crees y si los necesitas de nuevo, no volver
+
+
+def fibonacci_recursive(n: int) -> int:
+    """
+        Computes the n-th Fibonacci number.
+        :param n: n-th Fibonacci numner,
+        :return: Ther m-th Fibonacci number
+        """
+    if n < 0:
+        raise ValueError("n must be gerater than or equal to 0.")
+    if n < 2:
+        return n
+
+    if n in cache:
+        return cache[n]
+
+    result1 = fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    cache[n] = result1
+
+    return result1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('result1', type=int, help="Result Fibonacci number.")  # positional argument
+    args = parser.parse_args()
+
+    result = fibonacci_iterative(args.result1)
+    print(result)
